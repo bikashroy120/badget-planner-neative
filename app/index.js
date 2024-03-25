da61b6a37106fc17cf7aaa84128a6b0dc34b9258
bikashroy120 <bikashroydt@gmail.com>
2024-03-25 08:27:39 +0600

    login screen add

---
+++ app/index.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View } from "react-native";
+import services from "../utils/services";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+
+export default function Page() {
+
+  const router = useRouter()
+
+
+  useEffect(()=>{
+    isLogin()
+  },[])
+
+
+  const isLogin = async()=>{
+    const login = await services.getData("login")
+
+    console.log(login)
+
+    if(!login){
+      router.push("/login")
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
---
+++ app/login/index.jsx
@@ -0,0 +1,30 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+const LoginScreen = () => {
+  return (
+    <View className=" flex-1 bg-white ">
+      <Image
+        source={require("../../assets/image/unnamed.png")}
+        style={{ width: "100%", marginTop: 50, height: 400 }}
+      />
+
+      <View className=" bg-blue-700 flex-1 mt-[-50px] rounded-t-3xl p-5">
+        <Text className=" text-[30px] text-center text-white font-bold">
+          Personal Budget Planner
+        </Text>
+        <Text className=" text-white text-center text-[16px] mt-4 font-semibold">
+          Stay on Track Event by Event: Your Personal Budget Planner App!
+        </Text>
+        <TouchableOpacity className="w-full bg-white py-3 mt-7 rounded-full px-5">
+            <Text className=" text-center text-blue-500 font-semibold text-[20px]">Login/SignUp</Text>
+        </TouchableOpacity>
+        <Text className=" text-white font-semibold text-[14px] mt-4">*By login/signup you will agree to our teams and conditions</Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
